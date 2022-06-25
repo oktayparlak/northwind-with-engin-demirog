04848f834f634c8983c1e04da1213864b60bcdfc
Oktay P <oktay.parlak.12@hotmail.com>
2022-06-26 02:35:54 +0300

    26.06.2022--02:35

--- src/main/java/kodlamaio/northwind/api/controllers/UsersController.java
+++ src/main/java/kodlamaio/northwind/api/controllers/UsersController.java
@@ -2,13 +2,17 @@ package kodlamaio.northwind.api.controllers;
 
 import kodlamaio.northwind.business.abstracts.UserService;
 import kodlamaio.northwind.core.entities.User;
-import kodlamaio.northwind.core.utilities.results.Result;
+import kodlamaio.northwind.core.utilities.results.ErrorDataResult;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/users")
@@ -26,6 +30,18 @@ public class UsersController {
         return ResponseEntity.ok(this.userService.add(user));
     }
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorDataResult<?> handleValidationException(MethodArgumentNotValidException exceptions) {//Engin hoca <Object> verdi
 
+        Map<String, String> validationErrors = new HashMap<String, String>();
+
+        for (FieldError fieldError : exceptions.getBindingResult().getFieldErrors()) {
+            validationErrors.put(fieldError.getField(), fieldError.getDefaultMessage());
+        }
+
+        ErrorDataResult<Object> errors = new ErrorDataResult<Object>(validationErrors, "Doğrulama hataları");
+        return errors;
+    }
 
 }
